0dd252c98833b7fe913d19e1f1bd4beefbb0283b
mustafa91 <mustafa91@8767f4c6-5228-628b-d702-fcada9b4ab13>
2011-10-20 21:22:31 +0000

    Whole structure changed. Nothing works now. Just updating.

---
+++ WsFTP/src/in/mustafaak/wsftp/util/UserCreditentials.java
@@ -0,0 +1,20 @@
+package in.mustafaak.wsftp.util;
+
+/**
+ *
+ * @author Mustafa
+ */
+public class UserCreditentials {
+    public String user;
+    public String pass;
+
+    public UserCreditentials() {
+    }
+
+    
+    public UserCreditentials(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+}
